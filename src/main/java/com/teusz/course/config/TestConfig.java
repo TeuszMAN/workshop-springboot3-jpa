5f17b36b498cdfbc08bb73e64610b85d6d8806b1
Mateus Alves <gamertalverxd@gmail.com>
2025-01-09 11:29:22 -0300

    entity category

--- src/main/java/com/teusz/course/config/TestConfig.java
+++ src/main/java/com/teusz/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.teusz.course.entities.Category;
 import com.teusz.course.entities.Order;
 import com.teusz.course.entities.User;
 import com.teusz.course.entities.enums.OrderStatus;
+import com.teusz.course.repositories.CategoryRepository;
 import com.teusz.course.repositories.OrderRepository;
 import com.teusz.course.repositories.UserRepository;
 
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null,"Maria Brown", "maria@gmail.com", "98888888", "123456");
 		User u2 = new User(null,"Luquinhas da silva", "luquinha12@gmail.com", "921523123", "123456");
 		
