37b98456bcc9e6e12cd1f1c6f6601eda431003f6
Mateus Alves <gamertalverxd@gmail.com>
2025-01-10 10:55:33 -0300

    Entity Product

---
+++ src/main/java/com/teusz/course/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.teusz.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teusz.course.entities.Category;
+import com.teusz.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
