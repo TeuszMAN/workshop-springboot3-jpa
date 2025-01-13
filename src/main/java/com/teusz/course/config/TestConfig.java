c8d58f9cb8c33727649e2d3da1859f96b726e6fe
Mateus Alves <gamertalverxd@gmail.com>
2025-01-13 16:53:54 -0300

    Payment one-to-one association

--- src/main/java/com/teusz/course/config/TestConfig.java
+++ src/main/java/com/teusz/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.teusz.course.entities.Category;
 import com.teusz.course.entities.Order;
 import com.teusz.course.entities.OrderItem;
+import com.teusz.course.entities.Payment;
 import com.teusz.course.entities.Product;
 import com.teusz.course.entities.User;
 import com.teusz.course.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 
