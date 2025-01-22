e320d635ddbdd5e9122979e109257be9a2ba66c7
Mateus Alves <gamertalverxd@gmail.com>
2025-01-22 16:15:48 -0300

    Exception handling - update

--- src/main/java/com/teusz/course/services/UserService.java
+++ src/main/java/com/teusz/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.teusz.course.repositories.UserRepository;
 import com.teusz.course.services.exceptions.DataBaseException;
 import com.teusz.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new  ResourceNotFoundException(id);
+		}
+		
 	}
 	
 	public void updateData(User entity, User obj) {
